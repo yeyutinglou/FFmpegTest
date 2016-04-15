86a92ae8cbce47f50faccb6c86f1d07e51ea5657
516926914 <516926914@qq.com>
2016-04-15 09:57:28 +0800

    Player
    基于FFmpeg和Kxmovie的直播录播

---
+++ FFmpegTest/ViewController.m
@@ -0,0 +1,243 @@
+//
+//  ViewController.m
+//  FFmpegTest
+//
+//  Created by chenhairong on 15/5/3.
+//  Copyright (c) 2015年 times. All rights reserved.
+//
+
+#import "ViewController.h"
+#include "avformat.h"
+#include "avcodec.h"
+#import "KxMovieViewController.h"
+
+@interface ViewController ()
+{
+    NSArray *_localMovies;
+    NSArray *_remoteMovies;
+}
+@property (strong, nonatomic) UITableView *tableView;
+
+@end
+
+@implementation ViewController
+
+- (void)loadView
+{
+    self.view = [[UIView alloc] initWithFrame:[[UIScreen mainScreen] applicationFrame]];
+    self.tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
+    self.tableView.backgroundColor = [UIColor whiteColor];
+    //self.tableView.backgroundView = [[UIImageView alloc] initWithImage:image];
+    self.tableView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleBottomMargin;
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    self.tableView.separatorStyle = UITableViewCellSeparatorStyleSingleLine;
+    
+    [self.view addSubview:self.tableView];
+}
+
+- (BOOL)prefersStatusBarHidden { return YES; }
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.title = @"FFmpegPlayer";
+    self.tabBarItem = [[UITabBarItem alloc] initWithTabBarSystemItem:UITabBarSystemItemFeatured tag: 0];
+    
+    _remoteMovies = @[@"rtmp://192.168.1.154/live/stream231sub",
+                      @"rtmp://192.168.1.154:1935/live/stream231",
+                     @"rtmp://live.hkstv.hk.lxdns.com/live/hks",
+                   @"http://192.168.5.151:8080/test.mp4",
+                      @"http://192.168.5.151:8080/test1.mp4",
+                   @"http://192.168.5.151:8080/test2.flv",
+                     @"http://www.qeebu.com/newe/Public/Attachment/99/52958fdb45565.mp4",
+                    @"rtsp://192.168.5.230:8554/rtsp.aac",
+                     @"rtsp://admin:12345@192.168.2.222:554",
+                     @"rtsp://admin:jyd12345@192.168.2.231:554",
+                     @"rtsp://10.64.128.7:1935/vod/_definst_/mp4:/2016/4/8/2016488295228/video/2016488295228_2.mp4",
+                    @"http://192.168.0.104:8080/test1.mp4",
+                     @"http://192.168.0.104:8080/test.mp4"
+                      ];
+    
+
+#ifdef DEBUG_AUTOPLAY
+    [self performSelector:@selector(launchDebugTest) withObject:nil afterDelay:0.5];
+#endif
+    
+    [self.tableView reloadData];
+}
+
+- (void)launchDebugTest
+{
+    [self tableView:self.tableView didSelectRowAtIndexPath:[NSIndexPath indexPathForRow:4
+                                                                              inSection:1]];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    [self reloadMovies];
+    [self.tableView reloadData];
+}
+
+- (void) reloadMovies
+{
+    NSMutableArray *ma = [NSMutableArray array];
+    NSFileManager *fm = [[NSFileManager alloc] init];
+    NSString *folder = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,
+                                                            NSUserDomainMask,
+                                                            YES) lastObject];
+    NSArray *contents = [fm contentsOfDirectoryAtPath:folder error:nil];
+    
+    for (NSString *filename in contents) {
+        
+        if (filename.length > 0 &&
+            [filename characterAtIndex:0] != '.') {
+            
+            NSString *path = [folder stringByAppendingPathComponent:filename];
+            NSDictionary *attr = [fm attributesOfItemAtPath:path error:nil];
+            if (attr) {
+                id fileType = [attr valueForKey:NSFileType];
+                if ([fileType isEqual: NSFileTypeRegular] ||
+                    [fileType isEqual: NSFileTypeSymbolicLink]) {
+                    
+                    NSString *ext = path.pathExtension.lowercaseString;
+                    
+                    if ([ext isEqualToString:@"mp3"] ||
+                        [ext isEqualToString:@"caff"]||
+                        [ext isEqualToString:@"aiff"]||
+                        [ext isEqualToString:@"ogg"] ||
+                        [ext isEqualToString:@"wma"] ||
+                        [ext isEqualToString:@"m4a"] ||
+                        [ext isEqualToString:@"m4v"] ||
+                        [ext isEqualToString:@"wmv"] ||
+                        [ext isEqualToString:@"3gp"] ||
+                        [ext isEqualToString:@"mp4"] ||
+                        [ext isEqualToString:@"mov"] ||
+                        [ext isEqualToString:@"avi"] ||
+                        [ext isEqualToString:@"mkv"] ||
+                        [ext isEqualToString:@"mpeg"]||
+                        [ext isEqualToString:@"mpg"] ||
+                        [ext isEqualToString:@"flv"] ||
+                        [ext isEqualToString:@"vob"]) {
+                        
+                        [ma addObject:path];
+                    }
+                }
+            }
+        }
+    }
+    
+    // Add all the movies present in the app bundle.
+    NSBundle *bundle = [NSBundle mainBundle];
+    [ma addObjectsFromArray:[bundle pathsForResourcesOfType:@"mp4" inDirectory:@"SampleMovies"]];
+    [ma addObjectsFromArray:[bundle pathsForResourcesOfType:@"mov" inDirectory:@"SampleMovies"]];
+    [ma addObjectsFromArray:[bundle pathsForResourcesOfType:@"m4v" inDirectory:@"SampleMovies"]];
+    [ma addObjectsFromArray:[bundle pathsForResourcesOfType:@"wav" inDirectory:@"SampleMovies"]];
+    [ma addObjectsFromArray:[bundle pathsForResourcesOfType:@"avi" inDirectory:@"SampleMovies"]];
+    //[ma addObject:[bundle pathForResource:@"ios"ofType:@"mkv"]];
+    [ma sortedArrayUsingSelector:@selector(compare:)];
+    
+    _localMovies = [ma copy];
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 2;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    switch (section) {
+        case 0:     return @"Remote";
+        case 1:     return @"Local";
+    }
+    return @"";
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    switch (section) {
+        case 0:     return _remoteMovies.count;
+        case 1:     return _localMovies.count;
+    }
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellIdentifier = @"Cell";
+    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+                                      reuseIdentifier:cellIdentifier];
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    }
+    
+    NSString *path;
+    
+    if (indexPath.section == 0) {
+        
+        path = _remoteMovies[indexPath.row];
+        
+    } else {
+        
+        path = _localMovies[indexPath.row];
+    }
+    
+    cell.textLabel.text = path.lastPathComponent;
+    return cell;
+}
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSString *path;
+    NSMutableDictionary *parameters = [NSMutableDictionary dictionary];
+    
+    if (indexPath.section == 0) {
+        
+        if (indexPath.row >= _remoteMovies.count) return;
+        path = _remoteMovies[indexPath.row];
+        
+    } else {
+        
+        if (indexPath.row >= _localMovies.count) return;
+        path = _localMovies[indexPath.row];
+    }
+    
+    // increase buffering for .wmv, it solves problem with delaying audio frames
+    if ([path.pathExtension isEqualToString:@"wmv"])
+        parameters[KxMovieParameterMinBufferedDuration] = @(5.0);
+    
+    // disable deinterlacing for iPhone, because it's complex operation can cause stuttering
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)
+        parameters[KxMovieParameterDisableDeinterlacing] = @(YES);
+    
+    // disable buffering
+    //parameters[KxMovieParameterMinBufferedDuration] = @(0.0f);
+    //parameters[KxMovieParameterMaxBufferedDuration] = @(0.0f);
+    
+    KxMovieViewController *vc = [KxMovieViewController movieViewControllerWithContentPath:path
+                                                                               parameters:parameters];
+    [self presentViewController:vc animated:YES completion:nil];
+    //[self.navigationController pushViewController:vc animated:YES];    
+    
+   // LoggerApp(1, @"Playing a movie: %@", path);
+}
+
+@end
